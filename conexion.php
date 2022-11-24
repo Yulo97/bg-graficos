a448be37bd7ddb6dfd1f23a11dba5bf84de2395d
Giuliano De Vito <giuliano_devito@hotmail.com>
2022-11-24 13:40:55 -0300

    graficos php

--- conexion.php
+++ conexion.php
@@ -1,17 +1,24 @@
 <?php
 
-    $db = ['servidor' => '66.97.46.146','usuario' => 'bg_automotor_cli', 'password' => '94f8607915dff25f013e45fc0642fb9830b0fb25ab0ab46d477eaf1061def379','db' => 'db_bg_automotor'];
+try {
+	$con = mysqli_connect("66.97.46.146", "bg_automotor_cli", "94f8607915dff25f013e45fc0642fb9830b0fb25ab0ab46d477eaf1061def379", "db_bg_automotor") or die("Error en la conexion");
 
-  function Conectar($db)
-  {
-      try {
-          $conexion = new PDO("mysql:host={$db['servidor']};dbname={$db['db']};charset=utf8", $db['usuario'], $db['password']);          
-          $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-          return $conexion;
-      } catch (PDOException $e) {
-          exit($e->getMessage());
-      }
-  }
+	//$con = mysqli_connect("66.97.46.146","admin","admin40971232","db_asociados") or die("Error en la conexion");
+	//$con2 = mysqli_connect("66.97.46.146","admin","admin40971232","db_gestionblack") or die("Error en la conexion");
+
+	mysqli_set_charset($con, "utf8mb4");
+
+	/* Comprueba la conexión */
+	if (mysqli_connect_errno()) {
+		printf("Connect failed: %s\n", mysqli_connect_error());
+		exit();
+	}
+
+
+	/* Cierra la conexión */
+} catch (Exception $e) {
+	var_dump($e);
+}
 
 
 ?>
--- grafico-autos.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include ("./conexion.php");
-
-$conexion =  Conectar($db);
-
-$sql = mysqli_query($conexion, "CALL SP_BAL_AUTOS('".$_GET['sucursalID']."')") or die(mysqli_error($conexion));
