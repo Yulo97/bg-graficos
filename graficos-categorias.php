ff983154adbe419357280e03a460364b80c532d7
Giuliano De Vito <giuliano_devito@hotmail.com>
2022-11-25 14:10:37 -0300

    creacion nuevos graficos

---
+++ graficos-categorias.php
@@ -0,0 +1,14 @@
+<?php
+
+include ("./conexion.php");
+
+$arreglo = array();
+
+$sql = mysqli_query($con, "CALL SP_BAL_AUTOS('3')") or die(mysqli_error($con));
+
+while ($re = mysqli_fetch_array($sql)) {
+	$arreglo[] = $re;
+}
+
+mysqli_close($con);
+echo json_encode ($arreglo);
