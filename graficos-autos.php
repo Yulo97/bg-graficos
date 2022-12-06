072d197a9dfe480bedd75732e1b6bce80f550a11
Julian Bainotti <50033537+JuliBai@users.noreply.github.com>
2022-12-06 10:02:03 -0300

    Modificaciones graficos

--- graficos-autos.php
+++ graficos-autos.php
@@ -4,10 +4,9 @@ include ("./conexion.php");
 
 $arreglo = array();
 
-$sucursalID = $_GET['sucursalID'];
 //$sucursalID = "77de68daecd823babbb58edb1c8e14d7106e83bb";
 
-$sql = mysqli_query($con, "CALL SP_BAL_AUTOS('". $sucursalID ."')") or die(mysqli_error($con));
+$sql = mysqli_query($con, "CALL SP_BAL_AUTOS('". $_GET['sucursalID'] ."','". $_GET['tallerID'] ."')") or die(mysqli_error($con));
 
 while ($re = mysqli_fetch_array($sql)) {
 	$arreglo[] = $re;
